60fd1128caaf1c8d0208078b9bf83f8bc8f30ff3
Sam Walker <sgwalker327@gmail.com>
2023-04-05 00:01:25 -0600

    Created application controller current user method and added conditional to view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,7 @@ class UsersController < ApplicationController
   def login_user
     user = User.find_by(email: params[:email])
     if user.authenticate(params[:password])
+      session[:user_id] = user.id
       flash[:success] = "Welcome, #{user.email}!"
       redirect_to user_path(user)
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  enum role: %w(default admin)
 
 
   # def hosted_parties(user_id)
